9726a4d808d5a3c0dfc65fa5f7d62c1516df069c
Octav <octav@octav.name>
2020-11-10 14:46:14 +0200

    Updating to Sylius 1.8

--- src/DependencyInjection/Tavy315SyliusSalesPricesExtension.php
+++ src/DependencyInjection/Tavy315SyliusSalesPricesExtension.php
@@ -7,13 +7,33 @@ namespace Tavy315\SyliusSalesPricesPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class Tavy315SyliusSalesPricesExtension extends Extension
+final class Tavy315SyliusSalesPricesExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $config, ContainerBuilder $container): void
     {
         $this->processConfiguration($this->getConfiguration([], $container), $config);
         new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        if (!$container->hasExtension('doctrine_migrations') || !$container->hasExtension('sylius_labs_doctrine_migrations_extra')) {
+            return;
+        }
+
+        $container->prependExtensionConfig('doctrine_migrations', [
+            'migrations_paths' => [
+                'Tavy315\SyliusSalesPricesPlugin\Migrations' => '@Tavy315SyliusSalesPricesPlugin/Migrations',
+            ],
+        ]);
+
+        $container->prependExtensionConfig('sylius_labs_doctrine_migrations_extra', [
+            'migrations' => [
+                'Tavy315\SyliusSalesPricesPlugin\Migrations' => [ 'Sylius\Bundle\CoreBundle\Migrations' ],
+            ],
+        ]);
     }
 }
