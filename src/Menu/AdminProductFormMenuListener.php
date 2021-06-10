4ee914ef61cdf339708ac95ac28b0395ab861c40
Octav <octav@octav.name>
2021-06-10 14:05:25 +0300

    Remove translator from menu listeners

--- src/Menu/AdminProductFormMenuListener.php
+++ src/Menu/AdminProductFormMenuListener.php
@@ -5,18 +5,9 @@ declare(strict_types=1);
 namespace Tavy315\SyliusSalesPricesPlugin\Menu;
 
 use Sylius\Bundle\AdminBundle\Event\ProductMenuBuilderEvent;
-use Symfony\Component\Translation\Translator;
 
 final class AdminProductFormMenuListener
 {
-    /** @var Translator */
-    private $translator;
-
-    public function __construct(Translator $translator)
-    {
-        $this->translator = $translator;
-    }
-
     public function addItems(ProductMenuBuilderEvent $event): void
     {
         $menu = $event->getMenu();
@@ -27,7 +18,7 @@ final class AdminProductFormMenuListener
 
         $menu->addChild('salesprice')
              ->setAttribute('template', '@Tavy315SyliusSalesPricesPlugin/Admin/ProductVariant/Tab/_salesPrice.html.twig')
-             ->setLabel($this->translator->trans('tavy315_sylius_sales_prices.ui.sales_prices'))
+             ->setLabel('tavy315_sylius_sales_prices.ui.sales_prices')
              ->setLabelAttribute('icon', 'dollar');
     }
 }
